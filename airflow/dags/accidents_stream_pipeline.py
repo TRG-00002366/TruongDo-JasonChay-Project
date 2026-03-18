1517004fd09f5de1d50bf05ae26a12224a3e6e1b
TruongDo188 <Truongdo188@gmail.com>
2026-03-18 14:54:40 -0400

    dag

--- airflow/dags/accidents_stream_pipeline.py
+++ airflow/dags/accidents_stream_pipeline.py
@@ -1,46 +1,89 @@
 from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+from airflow.operators.python import PythonOperator
 from airflow.operators.bash import BashOperator
+from airflow.sensors.filesystem import FileSensor
 from datetime import datetime, timedelta
+import os
 
 default_args = {
     "owner": "accidents",
-    "retries": 3,
-    "retry_delay": timedelta(minutes=2),
+    "retries": 2,
+    "retry_delay": timedelta(minutes=5),
+    "email_on_failure":True,
 }
+def check_kafka_topic():
+    topic = "us_accidents"
+    if not topic:
+        raise ValueError("No Kafka Topic")
+    print("Kafka topic passed")
 
-# Parameterized values
-PRODUCER_SCRIPT = "/opt/airflow/kafka/producer.py"
-CONSUMER_SCRIPT = "/opt/airflow/spark/stream_consumer.py"
-KAFKA_TOPIC = "us_accidents"
-OUTPUT_PATH = "/opt/airflow/outputs"
-BOOTSTRAP_SERVER = "kafka:9092"
+def validate_output():
+    if not os.path.exists("/opt/airflow/outputs"):
+        raise ValueError("Output Folder missing")
+    print("Output validation passed")
 
 with DAG(
     dag_id="accidents_stream_pipeline",
     start_date=datetime(2026, 3, 1),
-    schedule="* * * * *",  
+    schedule="@daily",  
     catchup=False,
     default_args=default_args,
     tags=["airflow", "kafka", "spark", "accidents"],
 ) as dag:
 
-    run_producer = BashOperator(
-        task_id="run_producer",
-        bash_command=(
-            f"python {PRODUCER_SCRIPT} "
-            f"--topic {KAFKA_TOPIC} "
-            f"--bootstrap-servers {BOOTSTRAP_SERVER}"
-        ),
+    start = EmptyOperator(
+        task_id="start"
     )
 
-    run_consumer = BashOperator(
-        task_id="run_stream_consumer",
-        bash_command=(
-            f"python {CONSUMER_SCRIPT} "
-            f"--topic {KAFKA_TOPIC} "
-            f"--bootstrap-servers {BOOTSTRAP_SERVER} "
-            f"--output-path {OUTPUT_PATH}"
-        ),
+    check_kafka_topic_task = PythonOperator(
+        task_id="check_kafka_topic",
+        python_callable=check_kafka_topic
     )
 
-    run_producer >> run_consumer
+    run_streaming_job = BashOperator(
+        task_id="run_streaming_job",
+        bash_command=(
+            "spark-submit "
+            "--master spark://spark-master:7077 "
+            "--packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0 "
+            "/opt/airflow/spark/stream_consumer.py "
+            "--bootstrap-servers kafka:9094 "
+            "--duration 120 "
+            "--output-path /opt/airflow/outputs/raw/{{ ds }}"
+        )
+    )
+
+    wait_for_raw_data = FileSensor(
+        task_id="wait_for_raw_data",
+        filepath="outputs/raw",
+        poke_interval=30,
+        timeout=300
+    )
+
+    run_rdd_etl = BashOperator(
+        task_id="run_rdd_etl",
+        bash_command=(
+            "spark-submit "
+            "--master spark://spark-master:7077 "
+            "/opt/airflow/spark/batch_rdd_etl.py"
+        )
+    )
+
+    run_df_etl = BashOperator(
+        task_id="run_df_etl",
+        bash_command=(
+            "spark-submit "
+            "--master spark://spark-master:7077 "
+            "/opt/airflow/spark/batch_df_etl.py"
+        )
+    )
+
+    validate_output_task = PythonOperator(
+        task_id="validate_output",
+        python_callable=validate_output)
+
+    end = EmptyOperator(task_id="end")
+
+    start >> check_kafka_topic_task >> run_streaming_job >> wait_for_raw_data \
+        >> run_rdd_etl >> run_df_etl >> validate_output_task >> end
